34f16d22cacf7091fdb91f51e45dae7d88479cfd
Jebin Philipose <jebinphilip24@gmail.com>
2019-08-14 18:03:21 +0530

    Setup react-router-dom and add routes

--- src/app.js
+++ src/app.js
@@ -1,8 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
 // import your app here
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
+const ExpenseDashboardPage = () => (
+  <div>
+    This is ExpenseDashboardPage component
+  </div>
+);
+
+const CreateExpensePage = () => (
+  <div>
+    This is CreateExpensePage component
+  </div>
+);
+
+const EditExpensePage = () => (
+  <div>
+    This is EditExpensePage component
+  </div>
+);
+
+const HelpPage = () => (
+  <div>
+    This is HelpPage component
+  </div>
+);
+
+const routes = (
+    <BrowserRouter>
+      <Route path="/" component={ExpenseDashboardPage} exact={true} />
+      <Route path="/create" component={CreateExpensePage} />
+      <Route path="/edit" component={EditExpensePage} />
+      <Route path="/help" component={HelpPage} />
+    </BrowserRouter>
+);
+
 // render your app here
-ReactDOM.render(<p>ExpenseHub app is working!</p>, document.getElementById('app'));
+ReactDOM.render(routes, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,7 @@ module.exports = {
   },
   devtool: 'inline-source-map',
   devServer: {
-    contentBase: path.join(__dirname, '/public')
+    contentBase: path.join(__dirname, '/public'),
+    historyApiFallback: true
   }
 };
