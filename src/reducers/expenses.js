8f394ede612ce21959695135080fab225aac011a
Jebin Philipose <jebinphilip24@gmail.com>
2019-08-16 02:48:35 +0530

    Setup redux store for the app

---
+++ src/reducers/expenses.js
@@ -0,0 +1,20 @@
+const expensesDefaultState = [];
+
+export default (state = expensesDefaultState, action) => {
+  switch (action.type) {
+    case 'ADD_EXPENSE':
+      return [...state, action.expense]; // instead of state.concat(action.expense)
+    case 'REMOVE_EXPENSE':
+      return state.filter(({id}) => id !== action.id);
+    case 'EDIT_EXPENSE':
+      return state.map((expense) => {
+        if (expense.id === action.id) {
+          return {...expense, ...action.updates};
+        } else {
+          return expense;
+        };
+      });
+    default:
+      return state;
+  }
+};
