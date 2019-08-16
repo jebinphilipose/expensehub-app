9c596b1335bdf088de5cbf32624bc760c7029e3e
Jebin Philipose <jebinphilip24@gmail.com>
2019-08-17 00:06:11 +0530

    Setup react-redux and connect store with component

--- src/app.js
+++ src/app.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
+import {addExpense} from './action/expenses';
+import {setTextFilter} from './action/filters';
+import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 const store = configureStore();
-console.log(store.getState());
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+store.dispatch(addExpense({ description: 'Water Bill' }));
+store.dispatch(addExpense({ description: 'Gas Bill' }));
+store.dispatch(setTextFilter('water'));
+
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+console.log(visibleExpenses);
+
+const jsx = (
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
+);
+
+ReactDOM.render(jsx, document.getElementById('app'));
