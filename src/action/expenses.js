33483d369766ffe584f90e6d7b799c4a6ac12360
Jebin Philipose <jebinphilip24@gmail.com>
2019-08-19 03:23:02 +0530

    Return a promise for adding expense to firebase

--- src/action/expenses.js
+++ src/action/expenses.js
@@ -8,7 +8,7 @@ export const addExpense = (expense) => ({
 export const startAddExpense = ({description = '', note = '', amount = 0, createdAt = 0} = {}) => {
   return (dispatch) => {
     const expense = {description, note, amount, createdAt};
-    database.ref('expenses').push(expense).then((ref) => {
+    return database.ref('expenses').push(expense).then((ref) => {
       dispatch(addExpense({
         id: ref.key,
         ...expense
