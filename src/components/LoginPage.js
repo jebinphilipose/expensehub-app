648f333a31e728f00ace69936a4b84ed83e9cbd3
Jebin Philipose <jebinphilip24@gmail.com>
2019-08-22 23:02:51 +0530

    Setup button styles

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -7,7 +7,7 @@ const LoginPage = (props) => (
     <div className="box-layout__box">
       <h1 className="box-layout__title">ExpenseHub</h1>
       <p>It's time to get your expenses under control.</p>
-      <button onClick={props.startLogin}>Login</button>
+      <button className="button" onClick={props.startLogin}>Login with Google</button>
     </div>
   </div>
 );
